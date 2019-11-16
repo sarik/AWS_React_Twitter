02961a8fd4d918d4ef053d49baa3587be6598cf4
Sarik Siddiqui <user2@users-MacBook-Pro.local>
2019-11-17 05:24:05 +0530

    added one more table to make sure

--- server/index.js
+++ server/index.js
@@ -30,6 +30,10 @@ const pgClient = new Pool({
 pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
+  
+pgClient
   .query('CREATE TABLE IF NOT EXISTS public.user (firebaseID varchar PRIMARY key  ,firstname varchar,lastname varchar,created_at timestamp default now())')
   .catch(err => console.log(err,'couldnt create user'));
 

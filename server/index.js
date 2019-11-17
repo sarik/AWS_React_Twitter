ef0cecb899a93f90dffeb2e22a3a3a1276579093
Sarik Siddiqui <user2@users-MacBook-Pro.local>
2019-11-17 07:22:52 +0530

    deleting tables before dropping

--- server/index.js
+++ server/index.js
@@ -29,35 +29,55 @@ const pgClient = new Pool({
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient
+await pgClient
+  .query('drop table IF EXISTS user')
+  .then(cr => console.log('deleted  table user'))
+  .catch(err => console.log(err,'couldnt delete  user'));
+
+
+  await pgClient
+  .query('drop table IF EXISTS tweets')
+  .then(cr => console.log('deleted  table tweets'))
+  .catch(err => console.log(err,'couldnt delete  tweets'));
+
+  await pgClient
+  .query('drop table IF EXISTS followers')
+  .then(cr => console.log('deleted  table followers'))
+  .catch(err => console.log(err,'couldnt delete  followers'));
+
+
+
+await pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .then(cr => console.log('created values db'))
   .catch(err => console.log(err,'couldnt create values'));
 
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS user (firebaseID varchar PRIMARY key ,firstname varchar,lastname varchar,created_at timestamp default now())')
+await pgClient
+  .query('CREATE TABLE IF NOT EXISTS USER (firebaseID varchar PRIMARY key ,firstname varchar,lastname varchar,created_at timestamp default now())')
   .then(cr => console.log('created user db'))
   .catch(err => console.log(err,'couldnt create user'));
 
 
-pgClient
+await pgClient
   .query('CREATE TABLE IF NOT EXISTS tweets (id SERIAL PRIMARY key ,user_id varchar,post varchar(140),created_at timestamp default now())')
-  .catch(err => console.log(err));
+  .then(cr => console.log('created values tweet'))
+  .catch(err => console.log(err,'couldnt create tweets'));
 
-pgClient
+/* pgClient
   .query('ALTER TABLE tweets ADD CONSTRAINT fk_tweet_user_id FOREIGN KEY (user_id) REFERENCES user (firebaseID)')
   .catch(err => console.log(err));
+ */
 
-
-pgClient
+await pgClient
 .query('CREATE TABLE IF NOT EXISTS followers (id  varchar PRIMARY key ,followers varchar[],updated_at timestamp default now())')
-.catch(err => console.log(err));
+.then(cr => console.log('created values followers'))
+.catch(err => console.log(err,'couldnt create followers'));
 
 
-pgClient
+/* pgClient
   .query('ALTER TABLE followers ADD CONSTRAINT fk_follower_user_id FOREIGN KEY (id) REFERENCES user (firebaseID)')
   .catch(err => console.log(err));
-
+ */
 
 
 app.get('/current', async (req, res) => {
